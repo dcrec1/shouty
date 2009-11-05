70ca4baf71c2c1d53d5a66e47af17639a7f1e6a1
Diego Carrion <dc.rec1@gmail.com>
2009-11-05 02:50:53 -0200

    fixed create shouty feature story

--- features/enhanced/create_shouty.feature
+++ features/enhanced/create_shouty.feature
@@ -1,7 +1,8 @@
 Feature: Create Shouty
 
   Scenario: Home page
-    Given I am on the home page
+    Given I am logged
+    And I am on the home page
     When I fill in "hi, my name is what" for "shouty_body"
     And I press "Create Shouty"
     And I wait for the AJAX call to finish
--- features/enhanced/step_definitions/shouty_steps.rb
+++ features/enhanced/step_definitions/shouty_steps.rb
@@ -1,7 +1,16 @@
-Given /^I am on the home page$/ do
-  $browser.goto @host
+Given /^I am logged$/ do
+  goto signup_path
+  $browser.text_field(:id, "user_login").set($login = "dcrec1")
+  $browser.text_field(:id, "user_email").set($email = "dc.rec1@gmail.com")
+  $browser.text_field(:id, "user_password").set($password = "password")
+  $browser.text_field(:id, "user_password_confirmation").set($password)
+  $browser.button(:text, "Create User").click
 end
 
-Then /^a new shouty should be created$/ do
-  pending
+Given /^I am on the home page$/ do
+  goto root_path
+end
+
+def goto(path)
+  $browser.goto @host + path
 end
