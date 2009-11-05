9a0aaf1332bb5f95c9d9a7d223d7502b7e7999e8
Diego Carrion <dc.rec1@gmail.com>
2009-11-05 02:31:51 -0200

    fixed how specs requires spec_helper so metric_fu works

--- spec/controllers/shouties_controller_spec.rb
+++ spec/controllers/shouties_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ShoutiesController do
   should_have_before_filter :require_user
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
   should_be_authentic
