46e852b7513880068ac8b1ca35fb24e7c239c9df
Diego Carrion <dc.rec1@gmail.com>
2009-11-05 01:16:14 -0200

    users is authentic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  acts_as_authentic
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,5 @@
 require 'spec_helper'
 
 describe User do
-  before(:each) do
-    @valid_attributes = {
-      :login => "value for login",
-      :email => "value for email",
-      :crypted_password => "value for crypted_password",
-      :password_salt => "value for password_salt",
-      :persistence_token => "value for persistence_token",
-      :single_access_token => "value for single_access_token",
-      :perishable_token => "value for perishable_token"
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
-  end
+  should_be_authentic
 end
