74d1a86c0c581accc2de7d1e30196cec10e93b99
Rodrigo Matheus <rodrigorizando@gmail.com>
2009-11-11 17:30:42 -0200

    inputs margos e video na home

--- features/enhanced/step_definitions/shouty_steps.rb
+++ features/enhanced/step_definitions/shouty_steps.rb
@@ -4,7 +4,7 @@ Given /^I am logged$/ do
   $browser.text_field(:id, "user_email").set($email = "dc.rec1@gmail.com")
   $browser.text_field(:id, "user_password").set($password = "password")
   $browser.text_field(:id, "user_password_confirmation").set($password)
-  $browser.button(:text, "Create User").click
+  $browser.button(:text, "Create").click
 end
 
 Given /^I am on the home page$/ do
